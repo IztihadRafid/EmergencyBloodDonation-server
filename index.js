fa7f07ebc51a4a1f7380857e951571ac5324f6e0
IztihadRafid <iztihadrafid10@gmail.com>
2025-01-26 22:55:17 +0600

    verify Token, verifyAdmin added,jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -32,6 +33,51 @@ async function run() {
     const patientBloodRequestCollection = client.db('emergencyBloodDonationDB').collection('request_blood');// coming from request form
     const donationCollection = client.db('emergencyBloodDonationDB').collection('blood_donations'); // Changed collection name
 
+    //=============================================================
+    // JWT REALTED API
+    //=============================================================
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+      res.send({ token })
+    })
+
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token: ', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1]  //spliting token from bearer
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => { //verifying token
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
+
+    }
+
+
+    //=============================================================
+    //                VERIFY ADMIN
+    //=============================================================
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next()
+    }
+
+
+
+
     //-=================================
     //USER related APIS
     //-=================================
@@ -50,16 +96,46 @@ async function run() {
       res.send(result)
     })
 
+
+
+
     //===================================================
     //Getting All users from Db
     //===================================================
-    app.get('/user', async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email }
-      // console.log(query);
-      const result = await userCollection.find(query).toArray();
+    app.get('/user', verifyToken, verifyAdmin,async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result)
     })
+
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+
+    //=====================================================
+    //      MAKING ADMIN  
+    //=====================================================
+    app.patch('/user/admin/:id',verifyToken,verifyAdmin ,async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
 
     // Add this with your other user-related APIs
     // app.get('/user', async (req, res) => {
@@ -77,7 +153,7 @@ async function run() {
     //=============================================================
     //    DELETE NORMAL USER BY ADMIN FROM CLIENT (ALL USERs)
     //=============================================================
-    app.delete('/user/:id', async (req, res) => {
+    app.delete('/user/:id', verifyToken,verifyAdmin,async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
@@ -90,7 +166,7 @@ async function run() {
     app.delete('/requestblood/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
-      const result = await patientBloodRequestCollection .deleteOne(query);
+      const result = await patientBloodRequestCollection.deleteOne(query);
       res.send(result)
     })
 
